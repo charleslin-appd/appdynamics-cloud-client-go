bb11cf5429fc13c0d4949589946e6d22c97bd713
linchar <linchar@gmail.com>
2023-01-08 13:03:25 -0700

    Use ContextOAuth2 and clientcredential

--- main.go
+++ main.go
@@ -7,35 +7,39 @@ import (
 	"os"
 
 	appdcloudconnection_v1 "github.com/linchar/appd_cloud_api_client/connections_v1"
-
-	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/clientcredentials"
 )
 
 func main() {
-	ctx := context.Background()
-	conf := clientcredentials.Config{
+
+	// set up the struct according to clientcredentials package
+	ccConfig := clientcredentials.Config{
+
 		ClientID:     os.Getenv("APPD_CLOUD_CLIENT_ID"),
 		ClientSecret: os.Getenv("APPD_CLOUD_CLIENT_SECRET"),
 		TokenURL:     "https://lab1.observe.appdynamics.com/auth/dfdbdf71-7322-44d0-85fb-36c69a7c3789/default/oauth2/token",
-		AuthStyle:    oauth2.AuthStyleAutoDetect,
 	}
 
-	token, err := conf.Token(ctx)
+	// obtain the token
+	token, err := ccConfig.Token(context.Background())
+
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	// fmt.Println("oauth successful here is the access token ", token.AccessToken)
 
-	config := appdcloudconnection_v1.NewConfiguration()
-	fmt.Printf("%T\n", config)
-	config.AddDefaultHeader("Authorization", "Bearer "+token.AccessToken)
+	// Set up context with the token using API Context Key
+	// ContextOAuth2 takes a oauth2.TokenSource as authentication for the request.
+	auth := context.WithValue(context.Background(), appdcloudconnection_v1.ContextOAuth2, token)
 
-	client := appdcloudconnection_v1.NewAPIClient(config)
-	fmt.Printf("%T\n", client)
+	apiConfiguration := appdcloudconnection_v1.NewConfiguration()
+	// Assign the Configures's HTTPClient to credential client, otherwise
+	// it will default to the DefaultClient
+	apiConfiguration.HTTPClient = ccConfig.Client(auth)
+	// Create the API client with the configuration
+	client := appdcloudconnection_v1.NewAPIClient(apiConfiguration)
 
-	// resp, httpRes, err := client.ConnectionsApi.GetConnections(ctx).Execute()
-	resp, httpRes, err := client.ConnectionsApi.GetConnections(ctx, nil)
+	// make the call
+	resp, httpRes, err := client.ConnectionsApi.GetConnections(auth, nil)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
